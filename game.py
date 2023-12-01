66674711f4aed78e471802cd6a74fd72ba6372f6
noah WI <noah.windelhed@elev.ga.ntig.se>
2023-12-01 11:35:10 +0100

    Refactor mole drawing code

--- game.py
+++ game.py
@@ -16,37 +16,42 @@ mole_height = (screen_height-scoore_height-gutter*3)/3
 
 
 class Mole():
-    
+
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        
+
     def draw(self):
-        pygame.draw.rect(surface, color, pygame.Rect(self.x, self.y, mole_width, mole_height))
+        pygame.draw.rect(surface, color, pygame.Rect(
+            self.x, self.y, mole_width, mole_height))
+
 
 moles = [
     Mole(gutter, scoore_height),
     Mole(mole_width + gutter*2, scoore_height),
     Mole(mole_width*2 + gutter*3, scoore_height),
-    
+
     Mole(gutter, scoore_height + mole_height + gutter),
     Mole(mole_width + gutter*2, scoore_height + mole_height + gutter),
     Mole(mole_width * 2 + gutter*3, scoore_height + mole_height + gutter),
-    
+
     Mole(gutter, scoore_height + mole_height * 2 + gutter * 2),
     Mole(mole_width + gutter*2, scoore_height + mole_height * 2 + gutter * 2),
     Mole(mole_width * 2 + gutter*3, scoore_height + mole_height * 2 + gutter * 2),
 ]
 
+
 def draw_moles():
     for mole in moles:
         mole.draw()
-        
+
+
 surface = pygame.display.set_mode((screen_width, screen_height))
 
-color = (200,0,0)
+color = (200, 0, 0)
 
 # rec1 = pygame.draw.rect(surface, color, pygame.Rect(30, 30, 60, 60))
+
 
 def game():
     while True:
@@ -56,11 +61,12 @@ def game():
                 return
             elif event.type == MOUSEBUTTONDOWN:
                 print(event)
-                (x , y) = pygame.mouse.get_pos()
-                print(x, y)  
+                (x, y) = pygame.mouse.get_pos()
+                print(x, y)
 
         draw_moles()
-        
+
         pygame.display.flip()
+
 
 game()
